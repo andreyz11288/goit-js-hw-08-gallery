edcb2d5f32162f2ab1c271953cee4cd3dbd2af90
andreyz11288 <andreyz11288@gmail.com>
2020-10-07 21:51:08 +0300

    its work

--- js/work.js
+++ js/work.js
@@ -1,90 +1,14 @@
-//  <li class="gallery__item">
-//     <a
-//         class="gallery__link"
-//         href="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
-//     >
-//         <img
-//             class="gallery__image"
-//             src="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546__340.jpg"
-//             data-source="https://cdn.pixabay.com/photo/2010/12/13/10/13/tulips-2546_1280.jpg"
-//             alt="Tulips"
-//         />
-//     </a>
-// </li>
-const defaultEl = [
-  {
-    preview:
-      "https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2019/05/14/16/43/himilayan-blue-poppy-4202825_1280.jpg",
-    description: "Hokkaido Flower",
-  },
-  {
-    preview:
-      "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg",
-    description: "Container Haulage Freight",
-  },
-  {
-    preview:
-      "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg",
-    description: "Aerial Beach View",
-  },
-  {
-    preview:
-      "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg",
-    description: "Flower Blooms",
-  },
-  {
-    preview:
-      "https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg",
-    description: "Alpine Mountains",
-  },
-  {
-    preview:
-      "https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg",
-    description: "Mountain Lake Sailing",
-  },
-  {
-    preview:
-      "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg",
-    description: "Alpine Spring Meadows",
-  },
-  {
-    preview:
-      "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg",
-    description: "Nature Landscape",
-  },
-  {
-    preview:
-      "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg",
-    original:
-      "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
-    description: "Lighthouse Coast Sea",
-  },
-];
+import { default as defaultEl } from "./gallery-items.js";
 const ulRef = document.querySelector(".js-gallery");
 const divEl = document.querySelector(".js-lightbox");
 const btnEl = document.querySelector(".lightbox__button");
+const divModalEl = document.querySelector(".lightbox__content");
 
 const redEl = defaultEl.reduce((acc, el) => {
   return (acc += `<li class="gallery__item">
   <a class="gallery__link"  >
   <img class="gallery__image"
-  src="${el.original}"
+  src="${el.preview}"
   alt="${el.description}"
   />
   </a>
@@ -92,18 +16,34 @@ const redEl = defaultEl.reduce((acc, el) => {
 }, "");
 ulRef.innerHTML = redEl;
 
-const gallerEl = document.querySelector(".gallery__item");
-const linkEl = document.querySelector(".gallery__link");
 const imgEl = document.querySelector(".gallery__image");
 
 ulRef.addEventListener("click", e);
 function e(eve) {
-  console.log(eve.target);
   if (eve.target.className !== imgEl.className) {
     return;
   }
+  let element;
+  const bigImgEl = eve.target.alt;
+  for (let i = 0; i < defaultEl.length; i++) {
+    if (defaultEl[i].description === bigImgEl) {
+      element = defaultEl[i].original;
+    }
+  }
+
   divEl.classList.add("is-open");
+  divModalEl.innerHTML = `<img class="lightbox__image"
+    src="${element}"
+    alt="${bigImgEl}"
+  />`;
 }
 btnEl.addEventListener("click", () => {
   divEl.classList.remove("is-open");
+});
+
+const closeModalEl = document.querySelector('[data-action="close-lightbox"]');
+closeModalEl.addEventListener("click", () => {
+  const divCloseModal = document.querySelector(".lightbox__image");
+  divCloseModal.alt = "";
+  divCloseModal.src = "";
 });
